10e2c5c99b7851b7be3a8a5018b0e8e9e4928e13
richisil2005 <sanmigue2005@gmail.com>
2026-04-25 10:11:34 -0500

    2.1

--- src/main/java/pe/utec/academic/exception/Recursonoencontrado.java
+++
@@ -1,5 +0,0 @@
-package pe.utec.academic.exception;
- 
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) { super(message); }
-}
